3c56aa80bb2b51ba5eebc6e3f1377f3c9e18a3f2
James Hilliard <james.hilliard1@gmail.com>
2026-08-21 17:00:12 -0600

    selftests: tty: cover io_uring framed transmit
    Exercise framed serial uring commands without adding a liburing dependency.
    Cover unsupported TTYs, command and SQE validation, automatic blocking
    reissue, IOSQE_ASYNC, linked batch ordering, progress reporting, a
    maximum-sized frame, linked-timeout cancellation, and SQPOLL submission
    through the TOSTOP job-control check.
    
    Keep hardware transmit and cancellation tests opt-in through the existing
    serial framed transmit test environment.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- tools/testing/selftests/tty/serial_framed_tx_test.c
+++ tools/testing/selftests/tty/serial_framed_tx_test.c
@@ -9,9 +9,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
 #include <termios.h>
 #include <unistd.h>
 
+#include <linux/io_uring.h>
 #include <linux/serial.h>
 
 #include "../kselftest_harness.h"
@@ -40,6 +43,197 @@ static_assert(offsetof(struct serial_ioc_message_caps, max_timeout_ns) == 40,
 	      "max_timeout offset");
 static_assert(offsetof(struct serial_ioc_message_caps, reserved) == 48,
 	      "caps reserved offset");
+static_assert(SERIAL_URING_CMD_WRITE_MSG == 1, "uring command value");
+
+struct serial_uring {
+	int fd;
+	unsigned int setup_flags;
+	void *sq_ring;
+	void *cq_ring;
+	size_t sq_ring_size;
+	size_t cq_ring_size;
+	unsigned int sq_entries;
+	struct io_uring_sqe *sqes;
+	unsigned int *sq_head;
+	unsigned int *sq_tail;
+	unsigned int *sq_mask;
+	unsigned int *sq_flags;
+	unsigned int *sq_array;
+	unsigned int *cq_head;
+	unsigned int *cq_tail;
+	unsigned int *cq_mask;
+	struct io_uring_cqe *cqes;
+};
+
+static void serial_uring_destroy(struct serial_uring *ring)
+{
+	if (ring->sqes && ring->sqes != MAP_FAILED)
+		munmap(ring->sqes, ring->sq_entries * sizeof(*ring->sqes));
+	if (ring->cq_ring && ring->cq_ring != MAP_FAILED &&
+	    ring->cq_ring != ring->sq_ring)
+		munmap(ring->cq_ring, ring->cq_ring_size);
+	if (ring->sq_ring && ring->sq_ring != MAP_FAILED)
+		munmap(ring->sq_ring, ring->sq_ring_size);
+	if (ring->fd >= 0)
+		close(ring->fd);
+	memset(ring, 0, sizeof(*ring));
+	ring->fd = -1;
+}
+
+static int serial_uring_init_flags(struct serial_uring *ring,
+				   unsigned int flags)
+{
+	struct io_uring_params params = {
+		.flags = flags,
+		.sq_thread_idle = 1000,
+	};
+	void *sq_ring;
+	void *cq_ring;
+	int error;
+
+	memset(ring, 0, sizeof(*ring));
+	ring->fd = -1;
+	ring->fd = syscall(__NR_io_uring_setup, 4, &params);
+	if (ring->fd < 0)
+		return -errno;
+
+	ring->setup_flags = params.flags;
+	ring->sq_ring_size = params.sq_off.array +
+		params.sq_entries * sizeof(*ring->sq_array);
+	ring->sq_entries = params.sq_entries;
+	ring->cq_ring_size = params.cq_off.cqes +
+		params.cq_entries * sizeof(*ring->cqes);
+	if (params.features & IORING_FEAT_SINGLE_MMAP) {
+		if (ring->cq_ring_size > ring->sq_ring_size)
+			ring->sq_ring_size = ring->cq_ring_size;
+		ring->cq_ring_size = ring->sq_ring_size;
+	}
+
+	sq_ring = mmap(NULL, ring->sq_ring_size, PROT_READ | PROT_WRITE,
+		       MAP_SHARED, ring->fd, IORING_OFF_SQ_RING);
+	if (sq_ring == MAP_FAILED)
+		goto error;
+	ring->sq_ring = sq_ring;
+
+	if (params.features & IORING_FEAT_SINGLE_MMAP) {
+		cq_ring = sq_ring;
+	} else {
+		cq_ring = mmap(NULL, ring->cq_ring_size,
+			       PROT_READ | PROT_WRITE, MAP_SHARED,
+			       ring->fd, IORING_OFF_CQ_RING);
+		if (cq_ring == MAP_FAILED)
+			goto error;
+	}
+	ring->cq_ring = cq_ring;
+
+	ring->sqes = mmap(NULL, params.sq_entries * sizeof(*ring->sqes),
+			  PROT_READ | PROT_WRITE, MAP_SHARED, ring->fd,
+			  IORING_OFF_SQES);
+	if (ring->sqes == MAP_FAILED)
+		goto error;
+
+	ring->sq_head = sq_ring + params.sq_off.head;
+	ring->sq_tail = sq_ring + params.sq_off.tail;
+	ring->sq_mask = sq_ring + params.sq_off.ring_mask;
+	ring->sq_flags = sq_ring + params.sq_off.flags;
+	ring->sq_array = sq_ring + params.sq_off.array;
+	ring->cq_head = cq_ring + params.cq_off.head;
+	ring->cq_tail = cq_ring + params.cq_off.tail;
+	ring->cq_mask = cq_ring + params.cq_off.ring_mask;
+	ring->cqes = cq_ring + params.cq_off.cqes;
+
+	return 0;
+
+error:
+	error = errno;
+	serial_uring_destroy(ring);
+	return -error;
+}
+
+static int serial_uring_init(struct serial_uring *ring)
+{
+	return serial_uring_init_flags(ring, 0);
+}
+
+static struct io_uring_sqe *serial_uring_get_sqe(struct serial_uring *ring)
+{
+	unsigned int head = __atomic_load_n(ring->sq_head, __ATOMIC_ACQUIRE);
+	unsigned int tail = __atomic_load_n(ring->sq_tail, __ATOMIC_RELAXED);
+	unsigned int index;
+
+	if (tail - head >= ring->sq_entries)
+		return NULL;
+	index = tail & *ring->sq_mask;
+	memset(&ring->sqes[index], 0, sizeof(ring->sqes[index]));
+	ring->sq_array[index] = index;
+	__atomic_store_n(ring->sq_tail, tail + 1, __ATOMIC_RELEASE);
+
+	return &ring->sqes[index];
+}
+
+static int serial_uring_submit(struct serial_uring *ring, unsigned int count,
+			       int *results)
+{
+	unsigned int completed = 0;
+	unsigned int enter_flags = IORING_ENTER_GETEVENTS;
+	int ret;
+
+	if (__atomic_load_n(ring->sq_flags, __ATOMIC_ACQUIRE) &
+	    IORING_SQ_NEED_WAKEUP)
+		enter_flags |= IORING_ENTER_SQ_WAKEUP;
+	ret = syscall(__NR_io_uring_enter, ring->fd, count, count,
+		      enter_flags, NULL, 0);
+	if (ret < 0)
+		return -errno;
+	if (!(ring->setup_flags & IORING_SETUP_SQPOLL) && ret != (int)count)
+		return -EIO;
+
+	while (completed < count) {
+		unsigned int head = __atomic_load_n(ring->cq_head,
+						    __ATOMIC_RELAXED);
+		unsigned int tail = __atomic_load_n(ring->cq_tail,
+						    __ATOMIC_ACQUIRE);
+
+		while (head != tail && completed < count) {
+			struct io_uring_cqe *cqe =
+				&ring->cqes[head & *ring->cq_mask];
+
+			if (!cqe->user_data || cqe->user_data > count)
+				return -EIO;
+			results[cqe->user_data - 1] = cqe->res;
+			completed++;
+			head++;
+		}
+		__atomic_store_n(ring->cq_head, head, __ATOMIC_RELEASE);
+		if (completed == count)
+			break;
+		ret = syscall(__NR_io_uring_enter, ring->fd, 0,
+			      count - completed, IORING_ENTER_GETEVENTS,
+			      NULL, 0);
+		if (ret < 0)
+			return -errno;
+	}
+
+	return 0;
+}
+
+static struct io_uring_sqe *
+serial_uring_prep_message(struct serial_uring *ring, int fd,
+			  struct serial_ioc_message *message,
+			  unsigned int index)
+{
+	struct io_uring_sqe *sqe = serial_uring_get_sqe(ring);
+
+	if (!sqe)
+		return NULL;
+	sqe->opcode = IORING_OP_URING_CMD;
+	sqe->fd = fd;
+	sqe->cmd_op = SERIAL_URING_CMD_WRITE_MSG;
+	sqe->addr = (uintptr_t)message;
+	sqe->user_data = index + 1;
+
+	return sqe;
+}
 
 TEST(serial_framed_tx_layout)
 {
@@ -59,6 +253,29 @@ TEST(serial_framed_tx_rejects_pty)
 	EXPECT_EQ(errno, ENOTTY);
 	ASSERT_EQ(close(slave), 0);
 	ASSERT_EQ(close(master), 0);
+}
+
+TEST(serial_framed_tx_uring_rejects_pty)
+{
+	struct serial_ioc_message message = {};
+	struct serial_uring ring;
+	struct io_uring_sqe *sqe;
+	int results[1];
+	int ret;
+	int master;
+	int slave;
+
+	ret = serial_uring_init(&ring);
+	if (ret)
+		SKIP(return, "io_uring setup failed: %s", strerror(-ret));
+	ASSERT_EQ(openpty(&master, &slave, NULL, NULL, NULL), 0);
+	sqe = serial_uring_prep_message(&ring, slave, &message, 0);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	ASSERT_EQ(serial_uring_submit(&ring, 1, results), 0);
+	EXPECT_EQ(results[0], -EOPNOTSUPP);
+	ASSERT_EQ(close(slave), 0);
+	ASSERT_EQ(close(master), 0);
+	serial_uring_destroy(&ring);
 }
 
 static int serial_framed_tx_speed(speed_t *speed)
@@ -287,6 +504,118 @@ TEST(serial_framed_tx_validation)
 	ASSERT_EQ(close(fd), 0);
 }
 
+TEST(serial_framed_tx_uring_validation)
+{
+	struct serial_ioc_message_caps caps = {};
+	uint8_t frame = 0;
+	struct serial_ioc_transfer transfer = {
+		.tx_buf = (uintptr_t)&frame,
+		.len = sizeof(frame),
+	};
+	struct serial_ioc_message message = {
+		.version = SERIAL_MSG_ABI_VERSION,
+		.transfers = (uintptr_t)&transfer,
+		.transfer_count = 1,
+	};
+	struct serial_uring ring;
+	struct io_uring_sqe *sqe;
+	int results[1];
+	int ret;
+	int fd;
+
+	fd = open_framed_serial(&caps);
+	if (fd == -ENOENT)
+		SKIP(return, "SERIAL_FRAMED_TX_DEVICE is not set");
+	ASSERT_GE(fd, 0);
+	ret = serial_uring_init(&ring);
+	if (ret)
+		SKIP(goto out_close, "io_uring setup failed: %s",
+		     strerror(-ret));
+
+	sqe = serial_uring_prep_message(&ring, fd, &message, 0);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	sqe->cmd_op++;
+	ASSERT_EQ(serial_uring_submit(&ring, 1, results), 0);
+	EXPECT_EQ(results[0], -EOPNOTSUPP);
+
+	sqe = serial_uring_prep_message(&ring, fd, &message, 0);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	sqe->len = 1;
+	ASSERT_EQ(serial_uring_submit(&ring, 1, results), 0);
+	EXPECT_EQ(results[0], -EINVAL);
+
+	sqe = serial_uring_prep_message(&ring, fd, &message, 0);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	sqe->uring_cmd_flags = IORING_URING_CMD_FIXED;
+	ASSERT_EQ(serial_uring_submit(&ring, 1, results), 0);
+	EXPECT_EQ(results[0], -EINVAL);
+
+	sqe = serial_uring_prep_message(&ring, fd, &message, 0);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	sqe->buf_index = 1;
+	ASSERT_EQ(serial_uring_submit(&ring, 1, results), 0);
+	EXPECT_EQ(results[0], -EINVAL);
+
+	sqe = serial_uring_prep_message(&ring, fd, (void *)1, 0);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	sqe->flags = IOSQE_ASYNC;
+	ASSERT_EQ(serial_uring_submit(&ring, 1, results), 0);
+	EXPECT_EQ(results[0], -EFAULT);
+
+	serial_uring_destroy(&ring);
+out_close:
+	ASSERT_EQ(close(fd), 0);
+}
+
+TEST(serial_framed_tx_uring_sqpoll_tostop)
+{
+	struct serial_ioc_message_caps caps = {};
+	uint8_t frame = 0;
+	struct serial_ioc_transfer transfer = {
+		.tx_buf = (uintptr_t)&frame,
+		.len = sizeof(frame),
+	};
+	struct serial_ioc_message message = {
+		.version = SERIAL_MSG_ABI_VERSION,
+		.transfers = (uintptr_t)&transfer,
+		.transfer_count = 1,
+	};
+	struct serial_uring ring;
+	struct io_uring_sqe *sqe;
+	struct termios original;
+	struct termios settings;
+	int results[1];
+	int ret;
+	int fd;
+
+	if (!getenv("SERIAL_FRAMED_TX_URING_TEST"))
+		SKIP(return, "SERIAL_FRAMED_TX_URING_TEST is not set");
+
+	fd = open_framed_serial(&caps);
+	ASSERT_GE(fd, 0);
+	ASSERT_EQ(tcgetattr(fd, &original), 0);
+	settings = original;
+	settings.c_lflag |= TOSTOP;
+	ASSERT_EQ(tcsetattr(fd, TCSANOW, &settings), 0);
+
+	ret = serial_uring_init_flags(&ring, IORING_SETUP_SQPOLL);
+	if (ret)
+		SKIP(goto out_restore, "SQPOLL setup failed: %s",
+		     strerror(-ret));
+
+	sqe = serial_uring_prep_message(&ring, fd, &message, 0);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	ASSERT_EQ(serial_uring_submit(&ring, 1, results), 0);
+	EXPECT_EQ(results[0], 1);
+	EXPECT_EQ(message.completed_transfers, 1U);
+	EXPECT_EQ(message.completed_bytes, 1ULL);
+
+	serial_uring_destroy(&ring);
+out_restore:
+	ASSERT_EQ(tcsetattr(fd, TCSANOW, &original), 0);
+	ASSERT_EQ(close(fd), 0);
+}
+
 TEST(serial_framed_tx_write)
 {
 	static const uint8_t frame_a[] = "SERIAL-FRAME-A";
@@ -357,6 +686,148 @@ TEST(serial_framed_tx_write)
 	ASSERT_EQ(tcdrain(fd), 0);
 	free(large_frame);
 
+out_close:
+	ASSERT_EQ(close(fd), 0);
+}
+
+TEST(serial_framed_tx_uring_write)
+{
+	static const uint8_t frame_a[] = "SERIAL-URING-FRAME-A";
+	static const uint8_t frame_b[] = "SERIAL-URING-FRAME-B";
+	struct serial_ioc_message_caps caps = {};
+	struct serial_ioc_transfer first_transfers[] = {
+		{
+			.tx_buf = (uintptr_t)frame_a,
+			.len = sizeof(frame_a) - 1,
+			.guard_ns = 1000000,
+		}, {
+			.tx_buf = (uintptr_t)frame_b,
+			.len = sizeof(frame_b) - 1,
+			.guard_ns = 1000000,
+		},
+	};
+	struct serial_ioc_transfer second_transfer = {
+		.guard_ns = 1000000,
+	};
+	struct serial_ioc_message messages[] = {
+		{
+			.version = SERIAL_MSG_ABI_VERSION,
+			.transfers = (uintptr_t)first_transfers,
+			.transfer_count = ARRAY_SIZE(first_transfers),
+			.initial_guard_ns = 1000000,
+			.timeout_ns = 5000000000ULL,
+		}, {
+			.version = SERIAL_MSG_ABI_VERSION,
+			.transfers = (uintptr_t)&second_transfer,
+			.transfer_count = 1,
+			.initial_guard_ns = 1000000,
+			.timeout_ns = 5000000000ULL,
+		},
+	};
+	struct serial_uring ring;
+	struct io_uring_sqe *sqe;
+	uint8_t *large_frame;
+	size_t large_frame_len;
+	size_t first_bytes = sizeof(frame_a) + sizeof(frame_b) - 2;
+	int results[ARRAY_SIZE(messages)];
+	size_t i;
+	int ret;
+	int fd;
+
+	if (!getenv("SERIAL_FRAMED_TX_URING_TEST"))
+		SKIP(return, "SERIAL_FRAMED_TX_URING_TEST is not set");
+
+	fd = open_framed_serial(&caps);
+	ASSERT_GE(fd, 0);
+	ret = serial_uring_init(&ring);
+	if (ret)
+		SKIP(goto out_close, "io_uring setup failed: %s",
+		     strerror(-ret));
+
+	large_frame_len = caps.max_frame_bytes < 4096 ?
+			  caps.max_frame_bytes : 4096;
+	large_frame = malloc(large_frame_len);
+	ASSERT_NE((uintptr_t)large_frame, (uintptr_t)0);
+	for (i = 0; i < large_frame_len; i++)
+		large_frame[i] = i ^ 0x5a;
+	second_transfer.tx_buf = (uintptr_t)large_frame;
+	second_transfer.len = large_frame_len;
+
+	sqe = serial_uring_prep_message(&ring, fd, &messages[0], 0);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	/* Exercise automatic blocking reissue from the submission context. */
+	sqe->flags = IOSQE_IO_LINK;
+	sqe = serial_uring_prep_message(&ring, fd, &messages[1], 1);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	sqe->flags = IOSQE_ASYNC;
+	ASSERT_EQ(serial_uring_submit(&ring, ARRAY_SIZE(messages), results), 0);
+	EXPECT_EQ(results[0], (int)first_bytes);
+	EXPECT_EQ(results[1], (int)large_frame_len);
+	EXPECT_EQ(messages[0].completed_transfers,
+		  (uint32_t)ARRAY_SIZE(first_transfers));
+	EXPECT_EQ(messages[0].completed_bytes, (uint64_t)first_bytes);
+	EXPECT_EQ(messages[1].completed_transfers, 1U);
+	EXPECT_EQ(messages[1].completed_bytes, (uint64_t)large_frame_len);
+	ASSERT_EQ(tcdrain(fd), 0);
+
+	free(large_frame);
+	serial_uring_destroy(&ring);
+out_close:
+	ASSERT_EQ(close(fd), 0);
+}
+
+TEST(serial_framed_tx_uring_cancel)
+{
+	struct __kernel_timespec timeout = {
+		.tv_nsec = 20000000,
+	};
+	struct serial_ioc_message_caps caps = {};
+	uint8_t frame[] = "SERIAL-URING-CANCEL";
+	struct serial_ioc_transfer transfer = {
+		.tx_buf = (uintptr_t)frame,
+		.len = sizeof(frame) - 1,
+	};
+	struct serial_ioc_message message = {
+		.version = SERIAL_MSG_ABI_VERSION,
+		.transfers = (uintptr_t)&transfer,
+		.transfer_count = 1,
+		.initial_guard_ns = 1000000000ULL,
+		.timeout_ns = 5000000000ULL,
+	};
+	struct serial_uring ring;
+	struct io_uring_sqe *sqe;
+	int results[2];
+	int ret;
+	int fd;
+
+	if (!getenv("SERIAL_FRAMED_TX_URING_CANCEL_TEST"))
+		SKIP(return, "SERIAL_FRAMED_TX_URING_CANCEL_TEST is not set");
+
+	fd = open_framed_serial(&caps);
+	ASSERT_GE(fd, 0);
+	ret = serial_uring_init(&ring);
+	if (ret)
+		SKIP(goto out_close, "io_uring setup failed: %s",
+		     strerror(-ret));
+
+	sqe = serial_uring_prep_message(&ring, fd, &message, 0);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	sqe->flags = IOSQE_ASYNC | IOSQE_IO_LINK;
+	sqe = serial_uring_get_sqe(&ring);
+	ASSERT_NE((uintptr_t)sqe, (uintptr_t)0);
+	sqe->opcode = IORING_OP_LINK_TIMEOUT;
+	sqe->fd = -1;
+	sqe->addr = (uintptr_t)&timeout;
+	sqe->len = 1;
+	sqe->user_data = 2;
+
+	ASSERT_EQ(serial_uring_submit(&ring, 2, results), 0);
+	EXPECT_TRUE(results[0] == -ECANCELED || results[0] == -EINTR);
+	EXPECT_EQ(results[1], -ETIME);
+	EXPECT_EQ(message.completed_transfers, 0U);
+	EXPECT_EQ(message.completed_bytes, 0ULL);
+
+	serial_uring_destroy(&ring);
 out_close:
 	ASSERT_EQ(close(fd), 0);
 }
